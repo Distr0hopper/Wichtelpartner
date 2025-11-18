885353a1e6d6eb831e254556117286750597f54e
Distr0hopper <juliusarzberger@web.de>
2025-11-18 14:19:51 +0100

    Initial Commit

---
+++ Wichtelpartner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wichtelpartner
+//
+//  Created by Julius Arzberger on 18.11.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wichtelpartner/WichtelpartnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WichtelpartnerApp.swift
+//  Wichtelpartner
+//
+//  Created by Julius Arzberger on 18.11.25.
+//
+
+import SwiftUI
+
+@main
+struct WichtelpartnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
